bddc5d8b548b9b16ef7f85cc799982c598e84b52
halented <hallie.dunn1@gmail.com>
2018-12-23 19:56:02 -0800

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz (numba)
+if numba == Float
+  puts "nada"
+elsif (numba % 5 == 0) && (numba % 3 == 0)
+    "FizzBuzz"
+  elsif numba % 3 == 0
+    "Fizz"
+  elsif numba % 5 == 0
+    "Buzz"
+end
+end
